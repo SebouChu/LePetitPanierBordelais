fbc7ace950ed58bda500cf1999a21d7ea0c7a918
mharribey <mathieuharribey@hotmail.fr>
2017-10-05 15:01:15 +0200

    change color menu

--- app/assets/javascripts/burger.js
+++ app/assets/javascripts/burger.js
@@ -3,6 +3,7 @@ var burger = function(){
   var headerContent = document.getElementsByClassName('header-burger-content');
 
   header[0].addEventListener('click',function(e){
+    e.currentTarget.classList.toggle('color-red');
     headerContent[0].classList.toggle('display-none');
   });
 
