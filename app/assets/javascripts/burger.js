848a64eb1a121a9d7a8c736d33659ec891e575de
mharribey <mathieuharribey@hotmail.fr>
2017-10-05 12:38:24 +0200

    fix

--- app/assets/javascripts/burger.js
+++ app/assets/javascripts/burger.js
@@ -1,14 +1,9 @@
-document.addEventListener('DOMContentLoaded',function(){
-
+var burger = function(){
   var header = document.getElementsByClassName('header-burger');
   var headerContent = document.getElementsByClassName('header-burger-content');
-
-  if(window.innerWidth < 720){
-    header[0].classList.toggle("display-none");
-  }
 
   header[0].addEventListener('click',function(e){
     headerContent[0].classList.toggle('display-none');
   });
 
-});
+}
